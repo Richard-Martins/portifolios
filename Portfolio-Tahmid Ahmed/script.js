da812c24cb760e04430aee646eca271f04646405
Richard Martins <103905016+Richard-Martins@users.noreply.github.com>
2022-09-08 04:01:23 -0300

    deletado

--- Portfolio-Tahmid Ahmed/script.js
+++
@@ -1,9 +0,0 @@
-var sidebar = document.getElementById('sidebar')
-var menuLinks = document.getElementById('menu-links')
-
-sidebar.addEventListener('click', toggle)
-
-function toggle(){
-    sidebar.classList.toggle('active')
-    menuLinks.classList.toggle('active')
-}
