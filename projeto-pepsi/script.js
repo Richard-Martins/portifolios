80458d334eba4b29b7eb88b7389381be9c74585a
Richard Martins <103905016+Richard-Martins@users.noreply.github.com>
2022-09-09 09:19:28 -0300

    finalizando responsivo

--- projeto-pepsi/script.js
+++ projeto-pepsi/script.js
@@ -1,15 +1,16 @@
 var sidebar = document.getElementById('sidebar')
-var line = document.querySelector('.line')
-var navbar = document.querySelector('.navbar')
+var navlinks = document.getElementById('nav-links')
+var header = document.getElementById('header')
+var section = document.getElementById('landing-page')
 
 sidebar.addEventListener('click', toggle)
 
 function toggle(){
     sidebar.classList.toggle('active')
-    navbar.classList.toggle('active')
+    navlinks.classList.toggle('active')
+    header.classList.toggle('active')
+    section.classList.toggle('active')
 }
-
-
 
 function imgSlider (anything){
     document.querySelector('.pepsi').src = anything
