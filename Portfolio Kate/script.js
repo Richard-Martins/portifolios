37ada799e13b2951efb81ecb33bb92a2961fa0d5
Richard Martins <103905016+Richard-Martins@users.noreply.github.com>
2022-09-21 12:19:27 -0300

    fazendo responsivo

--- Portfolio Kate/script.js
+++ Portfolio Kate/script.js
@@ -1,7 +1,21 @@
-var moon = document.getElementById("moon");
+var navLinks = document.getElementById("nav-links");
+var darkmode = document.querySelector(".darkMode");
+var body = document.body;
 
-moon.addEventListener("click", toggle);
+darkmode.addEventListener("click", changeBg);
 
-function toggle() {
-  moon.classList.toggle("active");
+function changeBg() {
+  body.classList.toggle("active");
+}
+
+sidebar.addEventListener("click", toggleOpen);
+function toggleOpen() {
+  var sidebar = document.getElementById("sidebar");
+  navLinks.style.left = "0";
+}
+
+cross.addEventListener("click", toggleClose);
+function toggleClose() {
+  var cross = document.getElementById("cross");
+  navLinks.style.left = "-100%";
 }
