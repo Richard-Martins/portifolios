69875ae7326deb965c3c6a5beeff5fe6d5e65f88
Richard Martins <103905016+Richard-Martins@users.noreply.github.com>
2022-09-21 14:20:50 -0300

    finalizado

--- Portfolio Kate/script.js
+++ Portfolio Kate/script.js
@@ -1,21 +1,39 @@
-var navLinks = document.getElementById("nav-links");
-var darkmode = document.querySelector(".darkMode");
 var body = document.body;
+var openMenu = document.getElementById("open-menu");
+var closeMenu = document.getElementById("close-menu");
+var navLinks = document.getElementById("nav-links");
+var navbar = document.querySelector(".navbar");
+var titulo = document.querySelector(".titulo");
+var paragrafo = document.getElementsByTagName("p")[0];
+var btn = document.querySelector(".btn");
+var socialMedia = document.querySelector(".social-media");
+var changeBg = document.getElementById("changeBg");
+var conteiner = document.querySelector(".conteiner");
 
-darkmode.addEventListener("click", changeBg);
+openMenu.addEventListener("click", openNavbar);
+closeMenu.addEventListener("click", closeNavbar);
+conteiner.addEventListener("click", touchOut);
+changeBg.addEventListener("click", switchBg);
 
-function changeBg() {
+function switchBg() {
+  changeBg.classList.toggle("active");
   body.classList.toggle("active");
+  navLinks.classList.toggle("active");
+  openMenu.classList.toggle("active");
+  titulo.classList.toggle("active");
+  paragrafo.classList.toggle("active");
+  btn.classList.toggle("active");
+  socialMedia.classList.toggle("active");
 }
 
-sidebar.addEventListener("click", toggleOpen);
-function toggleOpen() {
-  var sidebar = document.getElementById("sidebar");
+function openNavbar() {
   navLinks.style.left = "0";
 }
 
-cross.addEventListener("click", toggleClose);
-function toggleClose() {
-  var cross = document.getElementById("cross");
+function closeNavbar() {
+  navLinks.style.left = "-100%";
+}
+
+function touchOut() {
   navLinks.style.left = "-100%";
 }
