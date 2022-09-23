eaacfc782b9f76b9fd968c077e46311d7eccbc93
Richard Martins <103905016+Richard-Martins@users.noreply.github.com>
2022-09-23 13:30:46 -0300

    tirei o responsivo

--- projeto-kfc/script.js
+++
@@ -1,9 +0,0 @@
-var menuIcon = document.querySelector(".menu-icon");
-var navLinks = document.getElementById("nav-links");
-
-menuIcon.addEventListener("click", toggleOpen);
-
-function toggleOpen() {
-  menuIcon.classList.toggle("active");
-  navLinks.classList.toggle("active");
-}
