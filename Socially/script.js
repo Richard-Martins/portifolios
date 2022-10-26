953a9d9539e7a3bc3ea184de370aa5035b802707
Richard Martins <103905016+Richard-Martins@users.noreply.github.com>
2022-10-26 18:24:50 -0300

    fazendo

---
+++ Socially/script.js
@@ -0,0 +1,11 @@
+// -----------------------DECLARANDO VARIAVÉIS-----------------
+
+var menuHamburger = document.getElementById("menu-hamburger");
+var menuX = document.getElementById("menu-x");
+
+// -----------------------FUNÇÃO ABRIR MENU-----------------
+
+menuHamburger.addEventListener("click", toggleOpen);
+function toggleOpen() {
+}
+// -----------------------FUNÇÃO FECHAR MENU-----------------
