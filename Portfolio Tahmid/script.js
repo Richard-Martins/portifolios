3ff7c3ce4beaf18764b80eacfad3a5aad0ec207f
Richard Martins <103905016+Richard-Martins@users.noreply.github.com>
2022-11-10 01:07:22 -0300

    finalizando

--- Portfolio Tahmid/script.js
+++ Portfolio Tahmid/script.js
@@ -8,5 +8,29 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("sticky");
   }
 });
-// ========SCROLL FUNCTION===============
+
 // =================SIDEBAR=================
+let navlist = document.getElementById("navlist");
+let menu = document.getElementById("menu");
+let menuX = document.getElementById("menu-x");
+
+menu.addEventListener("click", () => {
+  navlist.classList.add("active");
+  menu.style.display = "none";
+  menuX.style.display = "block";
+});
+
+menuX.addEventListener("click", () => {
+  navlist.classList.remove("active");
+  menu.style.display = "block";
+  menuX.style.display = "none";
+});
+
+// =================SIDEBAR SCROLL=================
+window.addEventListener("scroll", () => {
+  navlist.classList.remove("active");
+  menuX.style.display = "none";
+  menu.style.display = "block";
+});
+
+// ========SCROLL FUNCTION===============
